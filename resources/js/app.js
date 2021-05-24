daab76b71ab739ccbf633bcd1641d394abec5022
jakubjenigar <33846091+jakubjenigar@users.noreply.github.com>
2021-05-24 19:30:29 +0200

    added Vue compatibility

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,13 @@
 require('./bootstrap');
 
 require('alpinejs');
+
+import Vue from 'vue'
+window.Vue = require('vue');
+
+Vue.component('landing-page', require('./components/Landing.vue').default);
+
+const app = new Vue({
+    el: '#app',
+})
+
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::view('/landing', 'landing');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
