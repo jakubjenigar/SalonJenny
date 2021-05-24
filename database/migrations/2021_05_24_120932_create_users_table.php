655364856fc7f8d785b1229adf83537e8d59e0a9
jakubjenigar <33846091+jakubjenigar@users.noreply.github.com>
2021-05-24 17:06:01 +0200

    updated procfile to apache

--- database/migrations/2021_05_24_120932_create_users_table.php
+++ database/migrations/2021_05_24_120932_create_users_table.php
@@ -15,11 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('username');
             $table->string('password');
-            $table->rememberToken();
+            $table->string('email')->nullable();
+            $table->boolean('isAdmin');
+            $table->unsignedBigInteger('calendar_id');
+            $table->foreign('calendar_id')->references('id')->on('calendars');
             $table->timestamps();
         });
     }
