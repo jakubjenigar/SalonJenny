343a9be9d5cd17bd935d45d8f2543fd57ceea2d3
jakubjenigar <33846091+jakubjenigar@users.noreply.github.com>
2021-05-27 19:03:05 +0200

    added vue views

---
+++ resources/js/views/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-app class="change_font">
+
+        <!-- Sizes your content based upon application components -->
+        <v-main>
+            <nav-bar></nav-bar>
+            <!-- Provides the application the proper gutter -->
+            <v-container fluid class="ma-0 pa-0">
+                <!-- If using vue-router -->
+                <hero></hero>
+                <services></services>
+                <top-btn></top-btn>
+            </v-container>
+        </v-main>
+
+
+        <v-footer app>
+            <!-- -->
+            <v-col
+                class="text-center"
+                cols="12"
+                padless
+            >
+                <strong>©</strong> {{ new Date().getFullYear() }} — <strong>Salon Jenny</strong>
+            </v-col>
+        </v-footer>
+    </v-app>
+
+</template>
+
+<script>
+import NavBar from '../components/NavBar.vue';
+import Hero from '../views/Hero.vue';
+
+export default {
+    name: 'app',
+    components: {
+        NavBar,
+        Hero,
+    }
+}
+</script>
+
+
